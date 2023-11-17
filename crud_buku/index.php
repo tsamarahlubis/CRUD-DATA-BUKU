cce59a4c99d948a2fc0d84170e768d1ce62a32f5
tsamarahlubis <124503782+tsamarahlubis@users.noreply.github.com>
2023-11-17 22:48:41 +0700

    index.php

--- crud_buku/index.php
+++ crud_buku/index.php
@@ -12,14 +12,17 @@
       h1 {
         text-transform: uppercase;
         color: salmon;
+        font-size: 50px;
+
       }
     table {
       border: solid 1px #DDEEEE;
       border-collapse: collapse;
       border-spacing: 0;
-      width: 100%;
+      width: auto;
       margin: 10px auto 10px auto;
     }
+
     table thead th {
         background-color: #DDEFEF;
         border: solid 1px #DDEEEE;
@@ -28,21 +31,40 @@
         text-align: center;
         text-shadow: 1px 1px 1px #fff;
         text-decoration: none;
+        font-size: 20px;
     }
     table tbody td {
         border: solid 1px #DDEEEE;
         color: #333;
-        padding: 10px;
+        padding: 30px;
         text-shadow: 1px 1px 1px #fff;
         text-align: center;
+        
     }
     a {
           background-color: salmon;
           color: #fff;
           padding: 10px;
           text-decoration: none;
-          font-size: 12px;
+          border-radius: 0.5rem;
     }
+
+    a:hover {
+            background-color: peru;
+            color: #fff;
+        }
+
+    .action-buttons {
+        display: flex;
+        justify-content: space-between;
+}
+
+    .edit-button,
+    .delete-button {
+        margin-right: 5px;
+}
+
+
     </style>
   </head>
   <body>
@@ -78,7 +100,7 @@
       ?>
        <tr>
           <td><?php echo $no; ?></td>
-          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_buku']; ?>" style="width: 120px;"></td>
+          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_buku']; ?>" style="width: 100px;"></td>
           <td><?php echo $row['judul_buku']; ?></td>
           <td><?php echo $row['penulis_buku']; ?></td>
           <td><?php echo $row['tahun_buku']; ?></td>
@@ -86,9 +108,9 @@
           <td><?php echo $row['jenis_buku']; ?></td>
           <td>Rp <?php echo number_format($row['harga_buku'],0,',','.'); ?></td>
           
-          <td>
-              <a href="edit_produk.php?id=<?php echo $row['id_buku']; ?>">Edit</a> |
-              <a href="proses_hapus.php?id=<?php echo $row['id_buku']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+          <td> 
+              <a href="edit_produk.php?id=<?php echo $row['id_buku']; ?>">Edit</a> <br>
+              <a href="proses_hapus.php?id=<?php echo $row['id_buku']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a> 
           </td>
       </tr>
          
